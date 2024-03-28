c4334ce994c62243e56381ed3dc05a02b3401f67
DENIS DUPONT <denis.dupont@univ-evry.fr>
2024-03-28 13:49:10 +0100

    reactor js

--- app_server/controllers/utils/trans.js
+++ app_server/controllers/utils/trans.js
@@ -1,23 +1,42 @@
-const { courses  } = require('../../models/courses');
+const { courses } = require("../../models/courses");
 
-const programme = courses.reduce((a, { levels: levels, name }) => {
-    for (let level of levels) {
-        if (!a[level]) a[level] = [];
-        a[level].push(name);
+const menuMap = courses.reduce((acc, { levels, name }) => {
+  for (let level of levels) {
+    if (!acc.has(level)) {
+      acc.set(level, { title: level, subnav: [] });
     }
-    return a;
-}, {});
+    acc.get(level).subnav.push({ title: name });
+  }
+  return acc;
+}, new Map());
 
-
-const menu = Object.entries(programme).map(([level, courses]) => {
-    return {
-        title: level,
-        subnav: courses.map((course) => {
-            return { title: course }
-        })
-    }
-})
+const menu = Array.from(menuMap.values());
 
 module.exports = {
-    menu,
-}
+  menu,
+};
+
+// const menu = courses.reduce((acc, { levels, name }) => {
+//     for (let level of levels) {
+//         let menuItem = acc.find(item => item.title === level);
+//         if (!menuItem) {
+//             menuItem = { title: level, subnav: [] };
+//             acc.push(menuItem);
+//         }
+//         menuItem.subnav.push({ title: name });
+//     }
+//     return acc;
+// }, []);
+
+// const programme = courses.reduce((a, { levels, name }) => {
+//   for (let level of levels) {
+//     if (!a[level]) a[level] = [];
+//     a[level].push(name);
+//   }
+//   return a;
+// }, {});
+
+// const menu = Object.entries(programme).map(([level, courses]) => ({
+//   title: level,
+//   subnav: courses.map((course) => ({ title: course })),
+// }));
